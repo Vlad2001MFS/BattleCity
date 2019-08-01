7da6f6076e9782aeb6aba47ec63a84ea691c154f
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2019-08-01 20:01:08 +0300

    fix: calculating the texture coordinates in vertex shader without use the uv offset and uv size

--- data/shaders/simpleVS.glsl
+++ data/shaders/simpleVS.glsl
@@ -9,9 +9,11 @@ layout(binding = 0, std140) uniform ConstantBuffer {
 	mat4 gWorldMat;
 	mat4 gViewMat;
 	mat4 gProjMat;
+	vec2 gUvOffset;
+	vec2 gUvSize;
 };
 
 void main() {
 	gl_Position = gProjMat*gViewMat*gWorldMat*vec4(vsPos, 1.0);
-	psTexCoord = vsTexCoord;
+	psTexCoord = vsTexCoord*gUvSize + gUvOffset;
 }
